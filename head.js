4fe8d87620315e30cc08aa73f44144fd399a7720
Taomosquito <S_Peloquin123@live.com>
2024-05-17 14:08:15 -0400

    revise testing approach

--- head.js
+++ head.js
@@ -1,14 +1,15 @@
 // INPUT:
 // * Array into the new function as a parameter.
-// * String into the new function as a parameter.
+// * Setup to have  a test always return undefined.
 
 // OUTPUT: 
-// * First Index of the input string.
 // * First Index of the input array.
+// * Get undefined for an empty array.
 
 // DETAILS:
 // * Followed the best practice of startinng with the shell and planning stages.
 // * Set up Test Cases Before any implementation.
+// * No recommended stand in for undefined so decided to just check for undefined in testing.
 
 // assertEqual FUNCTION IMPLEMENTATION BELOW:
 const assertEqual = function(actual, expected) {
@@ -27,6 +28,4 @@ const head = function(arr) {
 // TESTING BELOW:
 assertEqual(head([5, 6, 7]), 5); // tests its ability to work on an array of numbers.
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Tests its abiliy to work on an array of strings 
-assertEqual(head("Hello"), "H"); // Tests its  ability to work on strings
-assertEqual(head("001"), "0"); // Tests its ability to work on string based numbers
-
+assertEqual(head([]), undefined); //checks that no value will return undefined.
