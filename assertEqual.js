04c36d60af5c68dd794e9bd80e08966c03051f9d
Taomosquito <S_Peloquin123@live.com>
2024-05-17 03:46:46 -0400

    revise

--- assertEqual.js
+++ assertEqual.js
@@ -7,15 +7,12 @@
 // * A passed message printed to the console.
 // * A useful testing function for later usage.
 
-// DETAILS:
-// * 
-
 // FUNCTION IMPLEMENTATION BELOW:
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✔ ✔ ✔ ✔ ✔ Assertion Passed: ${actual} === ${expected}`);
-  };
-  if (actual !== expected) {
+  }
+  else {
     console.log(`❌❌❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   };
 };
