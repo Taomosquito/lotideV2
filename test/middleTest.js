f536aa4179a15b8528b905d1c7e22f6cd27d1c3b
Taomosquito <S_Peloquin123@live.com>
2024-06-06 16:12:41 -0400

    switched to deeplyEqual logic and use the ready describe header

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,5 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
-const eqArrays = require('../eqArrays');
 
 // TESTING SET-UP BELOW:
 testArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -8,18 +7,18 @@ testArray2 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 shortTestArray1 = [1, 2, 3, 4, 5];
 shortTestArray2 = ["1", "2", "3", "4", "5"];
 
-describe("#tail", () => {
+describe("#middle", () => {
   it("confirms works with even length array and the number data type", () => {
-    assert.strictEqual(eqArrays(middle(testArray1), [5, 6]), true);
+    assert.deepEqual(middle(testArray1), [5, 6]);
   });
   it("confirms works with even length array and the string data type", () => {
-    assert.strictEqual(eqArrays(middle(testArray2), ["5", "6"]), true);
+    assert.deepEqual(middle(testArray2), ["5", "6"]);
   });
   it("confirms works with odd length array and the number data type", () => {
-    assert.strictEqual(eqArrays(middle(shortTestArray1), [3]), true);
+    assert.deepEqual(middle(shortTestArray1), [3]);
   });
   it("confirms works with odd length array and the string data type", () => {
-    assert.strictEqual(eqArrays(middle(shortTestArray2), ["3"]), true);
+    assert.deepEqual(middle(shortTestArray2), ["3"]);
   });
 
 });
