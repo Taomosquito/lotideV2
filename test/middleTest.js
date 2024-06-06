d76e060803b0ce4ae582b79c6eaa9e9cf5a6dc58
Taomosquito <S_Peloquin123@live.com>
2024-06-06 15:53:33 -0400

    refactor middle and tail complete

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,6 @@
-const assertArraysEqual = require("../assertArraysEqual");
-const middle = require("../middle");
+const assert = require('chai').assert;
+const middle = require('../middle');
+const eqArrays = require('../eqArrays');
 
 // TESTING SET-UP BELOW:
 testArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -7,8 +8,18 @@ testArray2 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 shortTestArray1 = [1, 2, 3, 4, 5];
 shortTestArray2 = ["1", "2", "3", "4", "5"];
 
-// TESTING BELOW:
-assertArraysEqual(middle(testArray1), [5, 6]);
-assertArraysEqual(middle(testArray2), ["5", "6"]);
-assertArraysEqual(middle(shortTestArray1), [3]);
-assertArraysEqual(middle(shortTestArray2), ["3"]);
+describe("#tail", () => {
+  it("confirms works with even length array and the number data type", () => {
+    assert.strictEqual(eqArrays(middle(testArray1), [5, 6]), true);
+  });
+  it("confirms works with even length array and the string data type", () => {
+    assert.strictEqual(eqArrays(middle(testArray2), ["5", "6"]), true);
+  });
+  it("confirms works with odd length array and the number data type", () => {
+    assert.strictEqual(eqArrays(middle(shortTestArray1), [3]), true);
+  });
+  it("confirms works with odd length array and the string data type", () => {
+    assert.strictEqual(eqArrays(middle(shortTestArray2), ["3"]), true);
+  });
+
+});
